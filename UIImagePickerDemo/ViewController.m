c4ebf80a0ebb012a82134b9e4529c6e5193d044a
xiaoxiaocainiao001 <liuwei_sun010@sina.com>
2015-12-02 11:15:31 +0800

    12.12
    摄像头获取照片与视频文件

--- UIImagePickerDemo/ViewController.m
+++ UIImagePickerDemo/ViewController.m
@@ -8,7 +8,8 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<UINavigationControllerDelegate, UIImagePickerControllerDelegate>
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
 
 @end
 
@@ -22,6 +23,80 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)takePicture:(id)sender
+{
+    
+    UIImagePickerController *picker = [[UIImagePickerController alloc] init];
+    
+    if([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
+        picker.sourceType = UIImagePickerControllerSourceTypeCamera;
+        NSArray *temp_MediaTypes = [UIImagePickerController availableMediaTypesForSourceType:picker.sourceType];
+        picker.mediaTypes = temp_MediaTypes;
+        picker.delegate = self;
+        picker.allowsImageEditing = YES;
+    }
+    
+    [self presentModalViewController:picker animated:YES];
+}
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info
+{
+    NSString *mediaType = [info objectForKey:UIImagePickerControllerMediaType];
+    
+    BOOL success;
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    NSError *error;
+    
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *documentsDirectory = [paths objectAtIndex:0];
+    
+    if ([mediaType isEqualToString:@"public.image"]){
+        
+        UIImage *image = [info objectForKey:@"UIImagePickerControllerEditedImage"];
+        ;    NSLog(@"found an image");
+        
+        NSString *imageFile = [documentsDirectory stringByAppendingPathComponent:@"temp.jpg"];
+        ;    NSLog(@"%@", imageFile);
+        
+        success = [fileManager fileExistsAtPath:imageFile];
+        if(success) {
+            success = [fileManager removeItemAtPath:imageFile error:&error];
+        }
+        
+        _imageView.image = image;
+        [UIImageJPEGRepresentation(image, 1.0f) writeToFile:imageFile atomically:YES];
+        
+        //SETIMAGE(image);
+        //CFShow([[NSFileManager defaultManager] directoryContentsAtPath:[NSHomeDirectory() stringByAppendingString:@"/Documents"]]);
+        ;    }
+    else if([mediaType isEqualToString:@"public.movie"]){
+        NSURL *videoURL = [info objectForKey:UIImagePickerControllerMediaURL];
+        NSLog(@"%@", videoURL);
+        NSLog(@"found a video");
+        NSData *videoData = [NSData dataWithContentsOfURL:videoURL];
+        
+        /****************************************/
+        
+        NSString *videoFile = [documentsDirectory stringByAppendingPathComponent:@"temp.mov"];
+        NSLog(@"%@", videoFile);
+        
+        success = [fileManager fileExistsAtPath:videoFile];
+        if(success) {
+            success = [fileManager removeItemAtPath:videoFile error:&error];
+        }
+        [videoData writeToFile:videoFile atomically:YES];
+        //CFShow([[NSFileManager defaultManager] directoryContentsAtPath:[NSHomeDirectory() stringByAppendingString:@"/Documents"]]);
+        ;    //NSLog(videoURL);
+    }
+    [picker dismissModalViewControllerAnimated:YES];
+}
+
+- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker {
+    
+    [picker dismissModalViewControllerAnimated:YES];
+
 }
 
 @end
